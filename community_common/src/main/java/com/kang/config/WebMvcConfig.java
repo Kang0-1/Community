41785c1533521e10f5d0536ad8c23b1a2a96ff52
kang <1215285086@qq.com>
2023-03-28 18:01:45 +0800

    webMvcConfig

--- community_common/src/main/java/com/kang/config/WebMvcConfig.java
+++ community_common/src/main/java/com/kang/config/WebMvcConfig.java
@@ -1,31 +1,54 @@
 package com.kang.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @author kang
+ * @description 集合 本地静态资源映射处理器 和 跨域处理
+ */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("file.upload.path")
     private String filePath;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true)
-                //GET,HEAD,POST,PUT,DELETE,OPTIONS
-                .allowedMethods("*")
-                //跨域允许最大时长
-                .maxAge(3600);
+    /**
+     * 通过CorsFilter解决跨域问题，防止添加拦截器后跨域失败
+     */
+    @Bean
+    public CorsFilter corsFilter(){
+        CorsConfiguration configuration=new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addExposedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return new CorsFilter(source);
     }
+
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("*")
+//                .allowCredentials(true)
+//                //GET,HEAD,POST,PUT,DELETE,OPTIONS
+//                .allowedMethods("GET", "POST", "DELETE", "PUT")
+//                //跨域允许最大时长
+//                .maxAge(3600);
+//    }
 
     /**
      * 配置本地资源映射路径
-     * @param registry
+     * @param registry z
      */
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
